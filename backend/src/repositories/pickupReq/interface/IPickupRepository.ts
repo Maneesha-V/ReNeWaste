7a519f1918697ef9219c03936fcf2d682e0d011f
Maneesha V <manu.maneeshav@gmail.com>
2025-04-20 21:14:36 +0530

    Ipickup repo interface

--- backend/src/repositories/pickupReq/interface/IPickupRepository.ts
+++ backend/src/repositories/pickupReq/interface/IPickupRepository.ts
@@ -1,7 +1,11 @@
 import { IPickupRequest, IPickupRequestDocument } from "../../../models/pickupRequests/interfaces/pickupInterface";
+import { PickupDriverFilterParams } from "../../../types/driver/pickupTypes";
 import { PickupFilterParams } from "../../../types/wastePlant/authTypes";
 import { IUpdatePickupRequest } from "../../../types/wastePlant/pickupTypes";
-
+export interface EnhancedPickup extends IPickupRequest {
+    userFullName: string;
+    selectedAddress: any; 
+  }
 export interface IPickupRepository {
     getPickupById(pickupReqId: string): Promise<IPickupRequestDocument>;
     createPickup(pickupData: Partial<IPickupRequest>): Promise<IPickupRequestDocument>;
@@ -9,4 +13,5 @@ export interface IPickupRepository {
     updatePickupStatusAndDriver(pickupReqId: string,updateData: {status: string; driverId: string; }): Promise<IPickupRequestDocument>;
     updatePickupRequest(pickupReqId: string, updatePayload: IUpdatePickupRequest): Promise<IPickupRequestDocument>;
     updatePickupDate(pickupReqId: string, updateData: any): Promise<IPickupRequestDocument>;
+    getPickupsByDriverId(filters: PickupDriverFilterParams): Promise<EnhancedPickup[]>;
 }
