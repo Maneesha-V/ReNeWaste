9a40125854cc2492d2c436e833e43b0a7a522bd6
Maneesha V <manu.maneeshav@gmail.com>
2025-05-05 21:46:05 +0530

    Deleted these files

--- backend/src/models/pickupResidential/pickupResModel.ts
+++
@@ -1,5 +0,0 @@
-import { model } from "mongoose";
-import { IPickupRequestResidentialDocument } from "./interfaces/pickupResInterface";
-import { pickupRequestResidentialSchema } from "./pickupResSchema";
-
-export const PickupResidentialModel =  model<IPickupRequestResidentialDocument>('PickupResidential', pickupRequestResidentialSchema);
--- backend/src/repositories/pickupResidential/interface/IPickupResRepository.ts
+++
@@ -1,6 +0,0 @@
-import { IPickupRequestResidential, IPickupRequestResidentialDocument } from "../../../models/pickupResidential/interfaces/pickupResInterface";
-
-export interface IPickupResidentialRepository {
-    createPickup(pickupData: Partial<IPickupRequestResidential>): Promise<IPickupRequestResidentialDocument>;
-
-}
