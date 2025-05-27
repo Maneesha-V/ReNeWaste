2ca732334e06c0cef7a004033c97890f10a678a6
Maneesha V <manu.maneeshav@gmail.com>
2025-05-27 13:01:49 +0530

    Change in formatDate in utils.

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -4,8 +4,8 @@ export function formatDateToDDMMYYYY(dateStr: string): string {
     const date = new Date(dateStr);
 
     if (isNaN(date.getTime())) return "Invalid date";
-    const month = String(date.getDate()).padStart(2, '0');
-    const day = String(date.getMonth() + 1).padStart(2, '0'); 
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); 
     const year = date.getFullYear();
     return `${day}-${month}-${year}`;
   }
